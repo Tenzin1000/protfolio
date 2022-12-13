021f18dbd15e4231b4351955dbe403e466c0cb83
sangey <sangeylama1000@gmail.com>
2022-12-13 17:07:01 +0545

    v2.0

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
+// coffee site
 var visitbtn=document.getElementById('show')
 var projectImg=document.getElementById('project-img')
 projectImg.addEventListener('mouseover',function(){
+    projectImg.style.filter='none';
+    visitbtn.style.padding='8px 20%';
     visitbtn.style.visibility='visible';
 
 });
 projectImg.addEventListener('mouseout',function(){
+    projectImg.style.filter='blur(1px)';
+    visitbtn.style.padding='8px 20px';
     visitbtn.style.visibility='hidden';
 })
+//corona data
 var visitbtn1=document.getElementById('show1')
 var projectImg1=document.getElementById('project-img1')
 projectImg1.addEventListener('mouseover',function(){
+    projectImg1.style.filter='none';
+    visitbtn1.style.padding='8px 20%';
     visitbtn1.style.visibility='visible';
 });
 projectImg1.addEventListener('mouseout',function(){
+    projectImg1.style.filter='blur(1px)';
+    visitbtn1.style.padding='8px 20px';
     visitbtn1.style.visibility='hidden';
+})
+//global tech
+var visitbtn2=document.getElementById('show2')
+var projectImg2=document.getElementById('project-img2')
+projectImg2.addEventListener('mouseover',function(){
+    projectImg2.style.filter='none';
+    visitbtn2.style.visibility='visible';
+    visitbtn2.style.padding='8px 20%';
+});
+projectImg2.addEventListener('mouseout',function(){
+    projectImg2.style.filter='blur(1px)';
+    visitbtn2.style.padding='8px 20px';
+    visitbtn2.style.visibility='hidden';
+})
+//trek nepal style for btn hide
+var visitbtn3=document.getElementById('show3')
+var projectImg3=document.getElementById('project-img3')
+projectImg3.addEventListener('mouseover',function(){
+    projectImg3.style.filter='none';
+    visitbtn3.style.padding='8px 20%';
+    visitbtn3.style.visibility='visible';
+});
+projectImg3.addEventListener('mouseout',function(){
+    visitbtn3.style.padding='8px 20px';
+    projectImg3.style.filter='blur(1px)';
+    visitbtn3.style.visibility='hidden';
 })
 //dark mode
 var moon =document.getElementById('moon');
